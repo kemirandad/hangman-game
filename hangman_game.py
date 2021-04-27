e12a62f205f10a98b202451de12e3aebd28bb85d
kemirandad <kjmd2s@gmail.com>
2021-04-27 16:27:38 -0500

    Agregando detalles estetico como mostrar el HangMan

--- hangman_game.py
+++ hangman_game.py
@@ -8,39 +8,63 @@ from hangman import HANGMAN_PICS
 def replace_letter_in_word(word):
     attemps_limit = len(word)
     word_as_list = list(word.lower())
+    
     hide_word = ["_" for letter in word]
+    
     for i in range(attemps_limit * 2):
-        count = attemps_limit * 2 - i
-        
-        tprint("Hangman--Game")
-        
-        print("¡BIENVENIDO A UN NUEVO JUEGO! \nLas siguientes son las reglas para ganar la partida")
-        print("1. Debes elegir una letra a la vez entre la A y la Z")
-        print("2. No está permitido el ingreso de números")
-        print(f'Tienes {count} intentos restantes...')
-        
-        for space in range(len(hide_word)):
-            print(hide_word[space], end=" ")
-        
-        try:
-            entry = input("\nIngresa una letra... ")
-            if entry.isalpha() != True:
-                raise ValueError('Oops! ha ocurrido un error...')
-            if len(entry) > 1:
-                raise Exception('Oops! ha ocurrido un error...')
-        
-            for i in range(len(word_as_list)):
-                if entry == word_as_list[i]:
-                    hide_word[i] = entry
-            os.system("clear")
-        except ValueError as ve:
-            print('\nNo es una letra')
-            print(ve)
-        except Exception as e:
-            print("\nSolo puedes ingresar una letra")
-            print(e)
+        if word_as_list != hide_word:
+            count = attemps_limit * 2 - i
+            
+            tprint("Hangman".center(50))
+            tprint("Game".center(50))
+            
+            show_hangman(HANGMAN_PICS, count, attemps_limit * 2)
+            
+            print("¡BIENVENIDO A UN NUEVO JUEGO! \nLas siguientes son las reglas para ganar la partida".center(50))
+            print("1. Debes elegir una letra a la vez entre la A y la Z".center(50))
+            print("2. No está permitido el ingreso de números".center(50))
+            print(f'Tienes {count} intentos restantes...'.center(50))
+            
+            for space in range(len(hide_word)):
+                print(hide_word[space], end=" ")
+            
+            try:
+                entry = input("\n\nIngresa una letra... ")
+                if entry.isalpha() != True:
+                    raise ValueError('Oops! ha ocurrido un error...')
+                if len(entry) > 1:
+                    raise Exception('Oops! ha ocurrido un error...')
+            
+                for i in range(len(word_as_list)):
+                    if entry == word_as_list[i]:
+                        hide_word[i] = entry
+                os.system("clear")
+            except ValueError as ve:
+                print('\nNo es una letra')
+                print(ve)
+            except Exception as e:
+                print("\nSolo puedes ingresar una letra")
+                print(e)
+        else:
+            break
     return False if "_" in hide_word else True
 
+def show_hangman(mans: list, attemps: int, attemps_n: int):
+    p_attemps = attemps / attemps_n
+    if p_attemps == 1.0:
+        print(mans[-1])
+    elif p_attemps >= 0.85:
+        print(mans[-2])
+    elif p_attemps >= 0.70:
+        print(mans[-3])
+    elif p_attemps >= 0.55:
+        print(mans[-4])
+    elif p_attemps >= 0.40:
+        print(mans[-5])
+    elif p_attemps >= 0.25:
+        print(mans[-6])
+    else:
+        print(mans[-7])
 
 def attemps():
     word_in_game = choose_word()
